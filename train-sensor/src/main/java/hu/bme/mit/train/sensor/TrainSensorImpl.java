085264daa42dd2469dbd6d16c35e04465493786d
Bendeguz Szekely <szekelybendeguz7@gmail.com>
2023-04-17 17:43:33 +0200

    feat: add alarm

--- train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
+++ train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
@@ -23,6 +23,15 @@ public class TrainSensorImpl implements TrainSensor {
 	@Override
 	public void overrideSpeedLimit(int speedLimit) {
 		this.speedLimit = speedLimit;
+		// new limit should be between 0 and 500, otherwise set alarm
+		if (speedLimit < 0 || speedLimit > 500) {
+			user.setAlarmState(true);
+		}
+
+		// set alarm if new speed limit is less than 50% of the reference speed
+		if (speedLimit < controller.getReferenceSpeed() * 0.5) {
+			user.setAlarmState(true);
+		}
 		controller.setSpeedLimit(speedLimit);
 	}
 
--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -3,17 +3,90 @@ package hu.bme.mit.train.sensor;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainUser;
+
 import static org.mockito.Mockito.*;
 
 public class TrainSensorTest {
 
+    // mock TrainController
+    TrainController controller;
+    // mock TrainUser
+    TrainUser user;
+    TrainSensorImpl sensor;
+    
     @Before
     public void before() {
         // TODO Add initializations
+        controller = mock(TrainController.class);
+        user = mock(TrainUser.class);
+        sensor = new TrainSensorImpl(controller, user);
     }
 
     @Test
     public void ThisIsAnExampleTestStub() {
         // TODO Delete this and add test cases based on the issues
     }
+
+    /**
+     * New speed limit is negative
+     * The alarm should be set
+     */
+    @Test
+    public void NewSpeedLimitIsNegative_SetsAlarm() {
+
+        sensor.overrideSpeedLimit(-1);
+        // at lease one time
+        verify(user, atLeast(1)).setAlarmState(true);
+    }
+
+    /**
+     * The new speed limit is over 500
+     * The alarm should be set
+     */
+    @Test
+    public void NewSpeedLimitIsOver500_SetsAlarm() {
+
+        sensor.overrideSpeedLimit(501);
+        verify(user, times(1)).setAlarmState(true);
+    }
+
+    /**
+     * The new speed limit is in range
+     * The alarm should not be set
+     */
+    @Test
+    public void NewSpeedLimitIsInRange_DoesNotSetAlarm() {
+
+        when(controller.getReferenceSpeed()).thenReturn(100);
+        sensor.overrideSpeedLimit(100);
+        verify(user, times(0)).setAlarmState(true);
+    }
+
+    /**
+     * The new speed limit is under 50% of the reference speed
+     * The alarm should be set
+     */
+    @Test
+    public void NewSpeedLimitIsUnder50PercentOfReferenceSpeed_SetsAlarm() {
+
+        when(controller.getReferenceSpeed()).thenReturn(100);
+        sensor.overrideSpeedLimit(49);
+        verify(user, times(1)).setAlarmState(true);
+    }
+
+    /**
+     * The new speed limit is over 50% of the reference speed
+     * The alarm should not be set
+     */
+    @Test
+    public void NewSpeedLimitIsOver50PercentOfReferenceSpeed_DoesNotSetAlarm() {
+
+        when(controller.getReferenceSpeed()).thenReturn(100);
+        sensor.overrideSpeedLimit(50);
+        verify(user, times(0)).setAlarmState(true);
+    }
+
 }
